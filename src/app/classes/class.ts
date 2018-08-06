c60b97f26e8b58bfe2d223c0c963a0d3c8fc32f9
Foster Stule <camodude5@yahoo.com>
2018-08-06 10:28:27 -0400

    added some class properities

--- src/app/classes/class.ts
+++ src/app/classes/class.ts
@@ -1,2 +1,12 @@
+import { Student } from "../students/student";
+
 export class Class {
+    Id : number;
+    Name : string;
+    Startdate: Date;
+    Enddate: Date;
+
+    Location: string;
+    Active: boolean;
+    Students: Student[];
 }
