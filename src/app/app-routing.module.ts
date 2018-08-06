fba0c2bc537c29ff389e05fbf388697e85cbdadb
Foster Stule <camodude5@yahoo.com>
2018-08-06 13:59:34 -0400

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClassesComponent } from './classes/classes.component';
+import { StudentsComponent } from './students/students.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'home',pathMatch:'Full'},
+  {path:'home', component: ClassesComponent},
+  {path:'class/:id', component: StudentsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
