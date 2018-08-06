eba0029c538cd545607ade071de1b878d761f19f
Foster Stule <camodude5@yahoo.com>
2018-08-06 10:27:02 -0400

    added a classes component and class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClassesComponent } from './classes/classes.component';
+import { StudentsComponent } from './students/students.component';
+import { CheckInComponent } from './Students/check-in/check-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClassesComponent,
+    StudentsComponent,
+    CheckInComponent
   ],
   imports: [
     BrowserModule,
