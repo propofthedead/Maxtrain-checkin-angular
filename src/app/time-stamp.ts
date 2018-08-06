8eb92903e67b25f1c45a650fce13419c64cdc803
Foster Stule <camodude5@yahoo.com>
2018-08-06 10:53:24 -0400

    added properties to the class

--- src/app/time-stamp.ts
+++ src/app/time-stamp.ts
@@ -1,2 +1,22 @@
+import { Student } from "./students/student";
+import { Class } from "./classes/class";
+
 export class TimeStamp {
+    Id: number;
+    FirstName: string;
+    LastName: string;
+    UserName: string;
+    Pin: string;
+    IsAdmin: boolean;
+    Active: boolean;
+    StudentId: number;
+    Student: Student;
+
+    ClassId: number;
+    Class: Class;
+
+    constructor(){
+        this.IsAdmin=false;
+        this.Active=true;
+    }
 }
